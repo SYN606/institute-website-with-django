3c637cdb3b816d040becb90973e27afb76d3546e
SYN <SYN@mail.com>
2022-12-12 19:52:57 +0530

    Added new css files

--- karamraji/urls.py
+++ karamraji/urls.py
@@ -6,9 +6,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('gallrey', include('gallrey.urls')),
     path('about', include('aboutUs.urls')),
     path('courses', include('courses.urls')),
+
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
