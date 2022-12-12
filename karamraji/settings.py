fa5086b71102ab21642befbcf35cf468d0833206
SYN <SYN@mail.com>
2022-12-12 19:29:27 +0530

    "edited media handller"

--- karamraji/settings.py
+++ karamraji/settings.py
@@ -103,8 +103,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
 
@@ -114,7 +112,9 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# handelling media 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- karamraji/urls.py
+++ karamraji/urls.py
@@ -1,7 +1,9 @@
 
-
 from django.contrib import admin
 from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +11,4 @@ urlpatterns = [
     path('gallrey', include('gallrey.urls')),
     path('about', include('aboutUs.urls')),
     path('courses', include('courses.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
