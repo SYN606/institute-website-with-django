51fa8df082091bdb8b3431c69df134f6e3f1f42f
SYN <SYN@mail.com>
2022-12-14 11:07:38 +0530

    adding css and data in html files

--- karamraji/urls.py
+++ karamraji/urls.py
@@ -14,4 +14,4 @@ urlpatterns = [
     path('courses', include('courses.urls')),
 
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
